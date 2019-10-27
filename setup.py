11ccc1de8818d56af68e06d676f25ffd4a350bef
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2019-10-27 14:45:48 +0100

    Add copyright

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+"""
+Implementation of the method proposed in the paper:
+'Certifiable Robustness to Graph Perturbations'
+Aleksandar Bojchevski and Stephan Günnemann, NeurIPS 2019
+
+Copyright (C) owned by the authors, 2019
+"""
 from setuptools import setup
 
 
